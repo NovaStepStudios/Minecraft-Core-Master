0f8a0e7051c75e5a9d2a64ae79ebbe8e444000c9
NovaStepStudios <stepnicka012@gmail.com>
2025-06-05 01:27:49 -0300

    Update MinecraftDownloader.js
    Level up XD

--- examples/MinecraftDownloader.js
+++ examples/MinecraftDownloader.js
@@ -1,6 +1,15 @@
-const {MinecraftDownloader} = require('./../components/DownloadMc');
+const { MinecraftDownloader } = require("./../components/DownloadMc.js");
 
 const Downloading = new MinecraftDownloader("./Minecraft", true, "release");
 
+Downloading.on("progress", (message) => {
+  console.log(`[Progreso] ${message}`);
+});
 
-Downloading.download();
+Downloading.on("error", (err) => {
+  console.error(`Error crítico: ${err}`);
+});
+
+Downloading.download("1.21.5").then(() => {
+  console.log("Descarga completada!");
+});
