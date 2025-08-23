e52860b56f3dfa1205cffa3b11da212bd760725d
NovaStepStudios <stepnicka012@gmail.com>
2025-08-23 14:58:16 -0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const MinecraftDownloader = require("./components/downloader/main.js");
-const MinecraftExecutor = require('./components/launch/index.js');
-const MinecraftLoaders = require('./components/downloader/index.js');
-
-module.exports = {MinecraftDownloader,MinecraftExecutor, MinecraftLoaders}
