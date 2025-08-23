9fc8f4ae8ed418eff2ac2ae78c3009831e9f8a74
NovaStepStudios <stepnicka012@gmail.com>
2025-08-23 14:58:58 -0300

    Delete test directory

--- test/Loaders/Quilt.js
+++
@@ -1,8 +0,0 @@
-const { MinecraftLoaders } = require('../../main.js');
-MinecraftLoaders.quilt({
-  root: './.minecraft',
-  version: '1.20',
-}).on('data', (msg) => {
-  console.log(`[Quilt] Descargando: ${msg.progress}/${msg.total}`);
-});
-// Link https://meta.quiltmc.org/v3/versions/game
--- test/components/Libraries.js
+++
@@ -1,9 +0,0 @@
-const MinecraftLibrariesDownloader = require("../../components/downloader/Minecraft/Minecraft-Libraries");
-
-const downloader = new MinecraftLibrariesDownloader("./.minecraft", "1.20.4");
-downloader.on("progress", ({ current, total, percent }) => {
-  console.log(`[${current}/${total}] Descargado - ${percent}%`);
-});
-downloader.on("done", () => console.log("✅ Librerías descargadas correctamente."));
-downloader.on("error", console.error);
-downloader.start();
